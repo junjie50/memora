a56e7993ac34af37a4cab9190df186d643020261
happylahlahlah <149375946+happylahlahlah@users.noreply.github.com>
2024-07-10 01:14:09 +0800

    feat: data from API, fixed UI

--- memora/src/pages/HotelListings.js
+++ memora/src/pages/HotelListings.js
@@ -1,76 +1,73 @@
 import Navbar from '../components/Navbar.js';
 import Footer from '../components/footer.js';
 import { useNavigate } from "react-router-dom";
-import React, { useState } from 'react';
-import logo from "../assets/memora.png"
+import React, { useState, useEffect } from 'react';
+import logo from "../assets/memora.png";
+import { retrieveAvailableHotels, retrieveStaticHotelDetailByHotelID } from '../services/ascenda-api.js';
 import './HotelListings.css';
 
 function HotelListings() {
   const navigate = useNavigate();
   const [priceRange, setPriceRange] = useState(52);
+  const [hotels, setHotels] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalHotels, setTotalHotels] = useState(0);
+  const hotelsPerPage = 5; // more means will retrieve more from APi -> can cause error
 
-  // Example hotel data array
-  const [hotels] = useState([
-    {
-      id: 1,
-      name: "Romantic Hotel",
-      description: "The Forest by Wangz is in the heart of Singapore, walking distance from Tan Tock Seng Hospital and United Square Mall.  This 4-star aparthotel is close to National Orchid Garden and Chinatown Heritage Center.",
-      price: 52,
-      imageUrl: "https://kaligo-web-expedia.imgix.net"
-    },
-    {
-      id: 2,
-      name: "New Majestic Hotel",
-      description: "A stay at New Majestic Hotel places you in the heart of Singapore, walking distance from NUS BaBa House and Baba House.  This 4-star hotel is close to Chinatown Heritage Center and <b>Universal Studios Singapore</b>®.",
-      price: 75,
-      imageUrl: "hotel-image.png"
-    },
-    {
-      id: 3,
-      name: "Mandarin Oriental Bangkok",
-      description: "Aenean laoreet, libero non eleifend viverra, elit nisl faucibus purus, eget viverra nulla sem vitae neque.",
-      price: 90,
-      imageUrl: "hotel-image-1.png"
-    },
-    {
-      id: 4,
-      name: "Romantic Hotel",
-      description: "The Forest by Wangz is in the heart of Singapore, walking distance from Tan Tock Seng Hospital and United Square Mall.  This 4-star aparthotel is close to National Orchid Garden and Chinatown Heritage Center.",
-      price: 52,
-      imageUrl: "hotel-image-2.jpg"
-    },
-    {
-      id: 5,
-      name: "New Majestic Hotel",
-      description: "A stay at New Majestic Hotel places you in the heart of Singapore, walking distance from NUS BaBa House and Baba House.  This 4-star hotel is close to Chinatown Heritage Center and <b>Universal Studios Singapore</b>®.",
-      price: 75,
-      imageUrl: "hotel-image-3.jpg"
-    },
-    {
-      id: 6,
-      name: "Mandarin Oriental Bangkok",
-      description: "Aenean laoreet, libero non eleifend viverra, elit nisl faucibus purus, eget viverra nulla sem vitae neque.",
-      price: 90,
-      imageUrl: "hotel-image.jpg"
-    },
-  ]);
-  const handleClick = () => {
-    navigate("/ViewHotelDetails")
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + '...';
+    }
+    return text;
   };
-  
+
+  const fetchHotels = async (page) => {
+    try {
+      console.log("Fetching hotel prices...");
+      const priceResponse = await retrieveAvailableHotels("WD0M", "2024-10-01", "2024-10-07", "en_US", "SGD", "SG", "2", "1");
+      const hotelPrices = priceResponse.data.hotels.slice((page - 1) * hotelsPerPage, page * hotelsPerPage);
+      setTotalHotels(priceResponse.data.hotels.length); // Set the total number of hotels
+      console.log("Hotel prices fetched:", hotelPrices);
+
+      console.log("Fetching hotel details...");
+      const hotelDetailsPromises = hotelPrices.map(hotel => retrieveStaticHotelDetailByHotelID(hotel.id));
+      const hotelDetailsResponses = await Promise.allSettled(hotelDetailsPromises);
+      const hotelDetails = hotelDetailsResponses
+        .filter(response => response.status === "fulfilled")
+        .map(response => response.value.data);
+
+      console.log("Hotel details fetched:", hotelDetails);
+
+      // combine prices and details
+      const detailedHotels = hotelPrices.map(hotel => {
+        const details = hotelDetails.find(detail => detail.id === hotel.id);
+        if (!details) {
+          console.warn(`Details not found for hotel ID: ${hotel.id}`);
+          return { ...hotel, name: hotel.name || "Unknown Hotel", rating: hotel.rating || 0, price: hotel.price || "N/A" };
+        }
+        return { ...hotel, ...details, description: truncateText(details.description, 380) };
+      });
+
+      console.log("Detailed hotels combined:", detailedHotels);
+      setHotels(detailedHotels);
+    } catch (error) {
+      console.error("Failed to fetch hotels", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchHotels(currentPage); // Fetch hotels when the currentpage loads
+  }, [currentPage]);
+
+  const handleClick = () => {
+    navigate("/ViewHotelDetails");
+  };
+
   const handlePriceChange = (e) => {
     setPriceRange(e.target.value);
   };
 
-  // For pagination
-  const [currentPage, setCurrentPage] = useState(1);
-  const hotelsPerPage = 5;
-
-  const indexOfLastHotel = currentPage * hotelsPerPage;
-  const indexOfFirstHotel = indexOfLastHotel - hotelsPerPage;
-  const currentHotels = hotels.slice(indexOfFirstHotel, indexOfLastHotel);
-  const totalPages = Math.ceil(hotels.length / hotelsPerPage);
-
+  //-----Pagination-----
   const handlePaginationClick = (value) => {
     if (value === 'prev') {
       setCurrentPage(currentPage => Math.max(1, currentPage - 1));
@@ -80,7 +77,23 @@ function HotelListings() {
       setCurrentPage(value);
     }
   };
+
+  const totalPages = Math.ceil(totalHotels / hotelsPerPage);
   
+  //to display only 7 page buttons at a time
+  const maxPageButtons = 7;
+  let startPage = Math.max(currentPage - Math.floor(maxPageButtons / 2), 1);
+  let endPage = Math.min(startPage + maxPageButtons - 1, totalPages);
+
+  if (endPage - startPage + 1 < maxPageButtons) {
+    startPage = Math.max(endPage - maxPageButtons + 1, 1);
+  }
+
+  const pageNumbers = [];
+  for (let number = startPage; number <= endPage; number++) {
+    pageNumbers.push(number);
+  }
+
   return (
     <div className="container">
       <Navbar />
@@ -92,10 +105,10 @@ function HotelListings() {
           <span className="link-text">2 Person</span> / 
           <span className="link-text">1 Room</span> / 
           <span className="link-text">24 Jul - 28 Jul</span>
-          </nav>
+        </nav>
       </header>
 
-      <div className="main-content"> {/* Added this div */}
+      <div className="main-content">
         <aside className="filter-section">
           <h2>Filter By</h2>
           <div className="filter-group">
@@ -163,49 +176,48 @@ function HotelListings() {
           </div>
 
           <div className="hotel-cards">
-            {currentHotels.map((hotel) => (
-              <div key={hotel.id} className="hotel-card">
-                <img src={hotel.imageUrl} alt="Hotel" className="hotel-image"/>
-                <div className="hotel-info">
-                  <div className="hotel-main-info">
-                    <h3>{hotel.name}</h3>
-                    <p>Bangkok · <a href="#">Show on map</a></p>
-                    <div className="horizontal-divider"></div>
-                    <p>{hotel.description}</p>
-                  </div>
-                  <div className="vertical-divider"></div>
-                  <div className="hotel-rating-price">
-                    <div className="hotel-rating">
-                      <span>★★★★★</span>
+            {hotels.map((hotel) => {
+              const imageUrl = hotel.image_details ? `${hotel.image_details.prefix}${hotel.default_image_index}${hotel.image_details.suffix}` : logo;
+              return (
+                <div key={hotel.id} className="hotel-card">
+                  <img src={imageUrl} alt={hotel.name} className="hotel-image"/>
+                  <div className="hotel-info">
+                    <div className="hotel-main-info">
+                      <h3>{hotel.name}</h3>
+                      <p>{hotel.original_metadata ? hotel.original_metadata.city : "Unknown City"} · <a href="#">Show on map</a></p>
+                      <div className="horizontal-divider"></div>
+                      <p dangerouslySetInnerHTML={{ __html: hotel.description }}></p>
                     </div>
-                    <div className="hotel-price">
-                      <p>Price per room per night from</p>
-                      <span>${hotel.price}</span>
-                      <button className="more-info" onClick={handleClick}>See more details</button>
+                    </div>
+                    <div className="vertical-divider"></div>
+                    <div className="hotel-rating-price">
+                      <div className="hotel-rating">
+                        <span>{hotel.rating}★</span>
+                      </div>
+                      <div className="hotel-price">
+                        <p>Price per room per night from</p>
+                        <span>${hotel.lowest_price}</span>
+                        <button className="more-info" onClick={handleClick}>See more details</button>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </main>
-      </div> {/* Closed this div */}
+      </div>
 
       <div className="pagination">
-        <button onClick={() => handlePaginationClick('prev')} disabled={currentPage === 1}>&laquo;</button>
-        {[...Array(totalPages)].map((_, index) => (
-          <button 
-            key={index + 1} 
-            onClick={() => handlePaginationClick(index + 1)}
-            className={currentPage === index + 1 ? 'active' : ''}
-          >
-            {index + 1}
+        <button onClick={() => handlePaginationClick('prev')} disabled={currentPage === 1}>Prev</button>
+        {pageNumbers.map(number => (
+          <button key={number} onClick={() => handlePaginationClick(number)} className={currentPage === number ? 'active' : ''}>
+            {number}
           </button>
         ))}
-        <button onClick={() => handlePaginationClick('next')} disabled={currentPage === totalPages}>&raquo;</button>
+        <button onClick={() => handlePaginationClick('next')} disabled={currentPage === totalPages}>Next</button>
       </div>
-    <Footer />
-  </div>
+      <Footer />
+    </div>
   );
 }
 
