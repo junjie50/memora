25dbf993ec7f188fb53d18bf72aa6e8d80454b74
MikeQih <2537175447@qq.com>
2024-07-14 23:09:38 +0800

    update Register Login function

--- config/db_memora.js
+++ config/db_memora.js
@@ -1,17 +1,40 @@
-const {MongoClient} = require('mongodb')
-//https://cloud.mongodb.com/v2/668e7133bf1a45356ff88220#/overview
-async function main(){
-    //Memora Cluster
-    const uri = "mongodb+srv://qihengchang1014:nmntY6pkVbZ9QfdV@memoracluster.nzggb9c.mongodb.net/?retryWrites=true&w=majority&appName=MemoraCluster"
+// //Configuration for connecting to MongoDB.
 
-    const client = new MongoClient(uri);
+// const {MongoClient} = require('mongodb')
 
-    try{
-        await client.connect();
-    } catch (e){
-        console.error(e);
-    } finally{
-        await client.close();
-    }   
-}
+// //https://cloud.mongodb.com/v2/668e7133bf1a45356ff88220#/overview
+// async function main(){
+//     //Memora Cluster
+//     const uri = "mongodb+srv://qihengchang1014:nmntY6pkVbZ9QfdV@memoracluster.nzggb9c.mongodb.net/?retryWrites=true&w=majority&appName=MemoraCluster"
 
+//     const client = new MongoClient(uri);
+
+//     try{
+//         await client.connect();
+//     } catch (e){
+//         console.error(e);
+//     } finally{
+//         await client.close();
+//     }   
+// }
+
+// module.exports = main;
+
+
+
+const mongoose = require('mongoose');
+
+const connectMongoDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://qihengchang1014:nmntY6pkVbZ9QfdV@memoracluster.nzggb9c.mongodb.net/memora', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected...');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectMongoDB;
--- server.js
+++ server.js
@@ -6,45 +6,58 @@
 // connectDB()
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+const cors = require('cors');
+// const bodyParser = require('body-parser');
+const connectMongoDB = require('./config/db_memora');
+// const { MongoClient } = require('mongodb');
+const memberRegisterRoute = require('./routes/MemberRegister');
 
 const app = express();
-const port = 3001;
+connectMongoDB();
+// const port = 3001;
 
-const uri = "mongodb+srv://qihengchang1014:nmntY6pkVbZ9QfdV@memoracluster.nzggb9c.mongodb.net/?retryWrites=true&w=majority&appName=MemoraCluster"
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// CORS middleware
+app.use(cors());
+
+// const uri = "mongodb+srv://qihengchang1014:nmntY6pkVbZ9QfdV@memoracluster.nzggb9c.mongodb.net/?retryWrites=true&w=majority&appName=MemoraCluster"
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // app.use(json());
 
-app.post('/register', async (req, res) => {
-    const { title, firstName, lastName, countryCode, phoneNumber, email, password } = req.body;
-    try {
-        await client.connect();
-        const database = client.db('MemoraCluster');
-        const collection = database.collection('Members');
+// app.post('/register', async (req, res) => {
+//     const { title, firstName, lastName, countryCode, phoneNumber, email, password } = req.body;
+//     try {
+//         await client.connect();
+//         const database = client.db('MemoraCluster');
+//         const collection = database.collection('Members');
         
-        const newMember = {
-            title,
-            firstName,
-            lastName,
-            countryCode,
-            phoneNumber,
-            email,
-            password
-        };
+//         const newMember = {
+//             title,
+//             firstName,
+//             lastName,
+//             countryCode,
+//             phoneNumber,
+//             email,
+//             password
+//         };
 
-        const result = await collection.insertOne(newMember);
+//         const result = await collection.insertOne(newMember);
 
-        res.status(201).json({ message: 'User registered successfully', userId: result.insertedId });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-    } finally {
-        await client.close();
-    }
-});
+//         res.status(201).json({ message: 'User registered successfully', userId: result.insertedId });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: 'Internal server error' });
+//     } finally {
+//         await client.close();
+//     }
+// });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+
+app.use(express.json());
+app.use('/api', memberRegisterRoute);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
