1d6c89ed1463cf76ddb0d216ae3ef5b2944fee98
MikeQih <2537175447@qq.com>
2024-07-14 19:49:49 +0800

    add cache_memora.js, ascenda-api_cache still work on, add Login/Register page

--- config/db_memora.js
+++ config/db_memora.js
@@ -1,4 +1,5 @@
 const {MongoClient} = require('mongodb')
+//https://cloud.mongodb.com/v2/668e7133bf1a45356ff88220#/overview
 async function main(){
     //Memora Cluster
     const uri = "mongodb+srv://qihengchang1014:nmntY6pkVbZ9QfdV@memoracluster.nzggb9c.mongodb.net/?retryWrites=true&w=majority&appName=MemoraCluster"
--- server.js
+++ server.js
@@ -1,6 +1,50 @@
 //File sets up and starts the server. (If can handle everything, no need app.js)
 //Includes the necessary configurations and middleware to handle incoming requests.
 
-import connectDB from './config/db_memora';
+// import connectDB from './config/db_memora';
 
-connectDB()
+// connectDB()
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 3001;
+
+const uri = "mongodb+srv://qihengchang1014:nmntY6pkVbZ9QfdV@memoracluster.nzggb9c.mongodb.net/?retryWrites=true&w=majority&appName=MemoraCluster"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// app.use(json());
+
+app.post('/register', async (req, res) => {
+    const { title, firstName, lastName, countryCode, phoneNumber, email, password } = req.body;
+    try {
+        await client.connect();
+        const database = client.db('MemoraCluster');
+        const collection = database.collection('Members');
+        
+        const newMember = {
+            title,
+            firstName,
+            lastName,
+            countryCode,
+            phoneNumber,
+            email,
+            password
+        };
+
+        const result = await collection.insertOne(newMember);
+
+        res.status(201).json({ message: 'User registered successfully', userId: result.insertedId });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
