57e42a26ac1c4dd8604804f542985b0887904067
Cai Junjie <junjie.50@hotmail.com>
2024-07-25 00:42:43 +0800

    minor edit to test server

--- backend/server.js
+++ backend/server.js
@@ -37,15 +37,6 @@ app.use(cors());
 // Set security HTTP headers
 app.use(helmet());
 
-// Limit request from the same API 
-const limiter = rateLimit({
-    max: 150,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too Many Request from this IP, please try again in an hour'
-});
-
-app.use('/api', limiter);
-
 // Body parser, reading data from body into req.body
 app.use(express.json({
     limit: '15kb'
