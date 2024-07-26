bb6f8d32ca3be3db4d4111f49a5e713f754d4a3b
Cai Junjie <junjie.50@hotmail.com>
2024-07-26 23:55:44 +0800

    feat: added integration call graph testing

---
+++ backend/test/booking_api.test.js
@@ -0,0 +1,113 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const {app, server} = require('../server')
+const Booking = require('../models/Booking')
+const Member = require('../models/Member')
+const {connectMongoDB, disconnectMongoDB} = require("../config/db_memora");
+const {compareDict} = require("../utils/modelUtils");
+var assert = require('assert');
+
+describe('Booking Route Integration Testing.', () => {
+    const api = supertest(app); 
+    beforeAll(async () => {
+      await connectMongoDB();
+    });
+  
+    beforeEach(async () => {
+      await Booking.deleteMany({}).exec();
+      await Member.deleteMany({}).exec();
+    })
+  
+    afterAll(async() => {
+      await Booking.deleteMany({}).exec();
+      await Member.deleteMany({}).exec();
+      await disconnectMongoDB();
+      server.close();
+    })
+    describe("Able to make booking with valid authorization.", () => {
+        it('Able to make booking and retrieve with valid authorization.', async () => {
+            const initialMember = 
+            {
+                username: "junjie50",
+                title: "mr",
+                firstName: "junjie",
+                lastName: "cai",
+                password: "123456",
+                email: "junjie50@@hotmail.com",
+                phoneNumber: "96650175",
+                address: "Upper Changi"
+            };
+
+            const credentials = {
+                username: "junjie50",
+                password: "123456"
+            };
+
+            const booking = {
+                "totalPayment" : 1234.45,
+                "creditCardNumber": "32424324432422342",
+                "cardExpiryDate" : "07/26",
+                "cvc": 236,
+                "destinationID": "diH7",
+                "specialRequest":  "Dog service",
+                "numberOfAdults": 3,
+                "numberOfChildren": 2,
+                "numberOfNights": 2,
+                "startDate": "2024-03-34",
+                "endDate": "2024-03-36",
+                "rooms": ["dsaf", "1231"]
+            };
+      
+            // user registered 
+            await api
+                .post('/api/users')
+                .send(initialMember)
+                .expect(201)
+                .expect('Content-Type', /application\/json/);
+        
+            // user login 
+            const res = await api
+                .post('/api/users/login')
+                .send(credentials)
+                .expect(200)
+                .expect('Content-Type', /application\/json/);
+
+            const token = res.body.token;
+        
+            // user failed to give Bearer token
+            await api
+                .post('/api/bookings')
+                .send(booking)
+                .expect(401)
+                .expect('Content-Type', /application\/json/);
+
+            // user gives valid autorization token
+            const savedBooking = await api
+                .post('/api/bookings')
+                .set({Authorization: `Bearer ${token}`})
+                .send(booking)
+                .expect(201)
+                .expect('Content-Type', /application\/json/);
+            
+            // user does not gives valid autorization token
+            const invalidBooking = await api
+                .get(`/api/bookings/${savedBooking.id}`)
+                .send(booking)
+                .expect(401)
+                .expect('Content-Type', /application\/json/);
+            
+                 // user does not gives valid autorization token
+            const resBooking = await api
+            .get(`/api/bookings/${savedBooking.id}`)
+            .set({Authorization: `Bearer ${token}`})
+            .send(booking)
+            .expect(401)
+            .expect('Content-Type', /application\/json/);
+
+            const validBooking = resBooking.body;
+
+            assert(compareDict(validBooking, booking));
+        });
+    });
+});
